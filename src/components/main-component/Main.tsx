7e6bec8b5f97bd978cf20b13269134d3902e76c8
iLinn <11552398+iLinn@users.noreply.github.com>
2025-02-19 19:10:44 +0200

    Added recipe request states with loader

--- src/components/main-component/Main.tsx
+++ src/components/main-component/Main.tsx
@@ -1,8 +1,12 @@
 import { JSX, useState } from 'react';
 import './Main.css';
+import Recipe from '../recipe-mock/Recipe';
+import Loading from '../loading/Loading';
 
 function MainContent() {
-  const [ingredientsList, setIngredientsList] = useState<Set<string>>(new Set([]))
+  const [ingredientsList, setIngredientsList] = useState<Set<string>>(new Set([]));
+  const [recipeShown, setRecipeShown] = useState(false);
+  const [recipeRequested, setRecipeRequested] = useState(false);
 
   function setIngerdients(ingredients: string[]): JSX.Element[] {
     return ingredients.map((ingredient) => (
@@ -29,35 +33,64 @@ function MainContent() {
     console.log('Added', ingredientsList, ingredient);
   }
 
+  function handleRequestRecipe(): void {
+    setRecipeRequested(true);
+    setTimeout(() => {
+      setRecipeShown(true);
+    }, 2000);
+  }
+
+  function handleResetForm(): void {
+    setIngredientsList(new Set([]));
+    setRecipeShown(false);
+    setRecipeRequested(false);
+  }
+
   return (
     <main className="flex-grow-1 box-shadow">
-      <form action={handleAddIngredient}
-        className="add-ingredient-form flex-row gap-1"
-        // onSubmit={handleAddIngredient}
-      >
-        <input
-          type="text"
-          placeholder="e.g. oregano"
-          aria-label="Add ingredient"
-          className="flex-grow-1"
-          name="ingredient"
-        />
-        <button 
-          className="plus-icon"
-        >Add ingredient</button>
-      </form>
-      {(ingredientsList.size > 0) && <section>
-        <h2>Ingredients on hand:</h2>
-        <ul className="ingredients-list" aria-live="polite">
-          {setIngerdients([...ingredientsList])}
-        </ul>
-        <div className="get-recipe-container flex-row gap-1 box-shadow">
-          <div>
-            <h3>Ready for a recipe?</h3>
-            <p>Generate a recipe from your list of ingredients.</p>
-          </div>
-          <button className="material-ui-button">Get a recipe</button>
-        </div>
+      {(!recipeShown && !recipeRequested) && <>
+        <form action={handleAddIngredient}
+          className="add-ingredient-form flex-row gap-1"
+          // onSubmit={handleAddIngredient}
+        >
+          <input
+            type="text"
+            placeholder="e.g. oregano"
+            aria-label="Add ingredient"
+            className="flex-grow-1"
+            name="ingredient"
+          />
+          <button 
+            className="plus-icon"
+          >Add ingredient</button>
+        </form>
+        {(ingredientsList.size > 0) && <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {setIngerdients([...ingredientsList])}
+          </ul>
+          {ingredientsList.size > 2 ? <div className="get-recipe-container flex-row gap-1 box-shadow">
+            <div>
+              <h3>Ready for a recipe?</h3>
+              <p>Generate a recipe from your list of ingredients.</p>
+            </div>
+            <button
+              onClick={handleRequestRecipe}
+              className="material-ui-button">
+                Get a recipe
+            </button>
+          </div> : <p className="fs-italic">Add more ingredients to get a recipe suggestion.</p>}
+        </section>}
+      </>}
+      {(recipeShown && recipeRequested) && <section>
+        <Recipe />
+        <button
+          className="material-ui-button"
+          onClick={handleResetForm}
+        >Get another recipe</button>
+      </section>}
+      {(!recipeShown && recipeRequested) && <section>
+        <Loading />
       </section>}
     </main>
   );
